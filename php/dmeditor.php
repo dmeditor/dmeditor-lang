bd038a4c3ec60e25b80170c1673d715a25ee0682
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-13 12:53:57 +0200

    continue

---
+++ php/dmeditor.php
@@ -0,0 +1,17 @@
+<?php
+class DMEditor{
+    /** render widget from template, return string. Invoked by DM Editor server */
+    public static function getWidgetTemplateString($templatePath, $styles){
+
+    }
+
+    /** render DM Editor to string. Invoked by php/php template */
+    public static function renderDMEditor(){
+
+    }
+
+    /** render DM Editor widget to string. Invoked by php rest api */
+    public static function renderDMEditorWidget(){
+
+    }    
+}
